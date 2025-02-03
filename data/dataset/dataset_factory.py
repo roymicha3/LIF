aaa38cf1aa08bbacfac07f0d3b23a53fc8c38661
roymicha3 <107579332+roymicha3@users.noreply.github.com>
2025-02-03 14:53:43 +0200

    dataset is now serializable

---
+++ data/dataset/dataset_factory.py
@@ -0,0 +1,27 @@
+from omegaconf import DictConfig
+
+from encoders.encoder import Encoder
+from settings.factory import Factory
+from settings.serializable import YAMLSerializable
+from data.dataset.dataset import DataType, OutputType
+
+# import all the datesets:
+from data.dataset.random_dataset import RandomDataset
+
+
+class DatasetFactory(Factory):
+    """
+    Factory class for creating kernels.
+    """
+    
+    @staticmethod
+    def create(name, config: DictConfig, 
+               data_type: DataType, 
+               output_type: OutputType, 
+               encoder: Encoder):
+        
+        return YAMLSerializable.get_by_name(name).from_config(name,
+                                                              config,
+                                                              data_type,
+                                                              output_type,
+                                                              encoder)
