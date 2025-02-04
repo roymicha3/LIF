48e9479fc74226626e99d414ffc8631db4975605
roymicha3 <roymicha3@gmail.com>
2025-02-04 21:11:27 +0200

    encoder factory

--- encoders/encoder_factory.py
+++ encoders/encoder_factory.py
@@ -3,17 +3,19 @@ from omegaconf import DictConfig
 from settings.factory import Factory
 from settings.serializable import YAMLSerializable
 
-# import all the spike encoders
+# import all encoders:
+from encoders.identity_encoder import IdentityEncoder
 from encoders.spike.latency_encoder import LatencyEncoder
 from encoders.spike.rate_encoder import RateEncoder
 
-class SpikeEncoderFactory(Factory):
+
+class EncoderFactory(Factory):
     """
-    Factory class for creating spike encoders.
+    Factory class for creating kernels.
     """
     @staticmethod
     def create(name: str, config: DictConfig, env_config: DictConfig):
         """
-        Create an instance of a registered spike encoder.
+        Create an instance of a registered kernel.
         """
         return YAMLSerializable.get_by_name(name).from_config(config, env_config)
