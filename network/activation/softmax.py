609266d2cf0b617ed7aa372bfbf984dfba56e307
roymicha3 <roymicha3@gmail.com>
2025-02-16 23:55:57 +0200

    now using activations

--- network/activation/softmax.py
+++ network/activation/softmax.py
@@ -8,13 +8,20 @@ class SoftmaxActivation(Activation, YAMLSerializable):
     def __init__(self):
         super().__init__()
         super(YAMLSerializable).__init__()
+        self.saved_vectors = None
 
-    def forward(self, input):
-        return F.softmax(input, dim=-1)
+    def forward(self, input_):
+        self.saved_vectors = input_
+        return F.softmax(input_, dim=-1)
 
-    def backward(self, input, grad_output):
-        softmax_output = F.softmax(input, dim=-1)
-        return grad_output * softmax_output * (1 - softmax_output)
+    def backward(self, grad_output):
+        if grad_output.dim() == 3:
+            grad_output = grad_output.squeeze(-1)
+        
+        input_ = self.saved_vectors 
+        softmax_output = F.softmax(input_, dim=-1)
+        grad = grad_output * softmax_output * (1 - softmax_output)
+        return grad.unsqueeze(-1)
     
     @classmethod
     def from_config(cls, config, env_config):
--- network/topology/neuron.py
+++ network/topology/neuron.py
@@ -2,6 +2,7 @@ import torch
 
 from network.kernel.kernel import Kernel
 from network.topology.connection import Connection
+from network.activation.activation import Activation
 
 class NeuronLayer(torch.nn.Module):
     """
@@ -10,22 +11,26 @@ class NeuronLayer(torch.nn.Module):
     
     def __init__(self,
                  kernel: Kernel,
-                 connection: Connection) -> None:
+                 connection: Connection,
+                 activation: Activation) -> None:
         
         super().__init__()
         
         self.kernel = kernel
         self.connection = connection
+        self.activation = activation
         
     
     def forward(self, input_):
         input_voltage = self.kernel.forward(input_)
         output = self.connection.forward(input_voltage)
+        output = self.activation.forward(output)
         return output
         
     def backward(self, output_grad):
         """
         Backward function for the neuron layer.
         """
-        input_grad = self.connection.backward(output_grad)
+        activation_grad = self.activation.backward(output_grad)
+        input_grad = self.connection.backward(activation_grad)
         return self.kernel.backward(input_grad)
